1d4338cfab5e9c0cf93182a302d495a32d4bbaa6
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-26 05:31:46 -0700

    init commit

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React, { FC } from 'react';
 import { View, Text, Button } from 'react-native';
 // import { ThemeProvider } from './src/theme/ThemeContext';
 import AppNavigator from './src/routes/index';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App: FC = () => {
   return (
     // <ThemeProvider>
-    <AppNavigator />
+    <HomeScreen />
     // </ThemeProvider>
   );
 };

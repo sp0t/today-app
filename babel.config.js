19cd047d900783ea6c8c06381d1b24aeddcd7f41
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-26 12:10:49 -0700

    [dev]-login page

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,5 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-reanimated/plugin',
-  ],
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["nativewind/babel"],
+  ]
 };

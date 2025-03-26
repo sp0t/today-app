fa08e7d5639913f3ce9329c768cd0a078681abe5
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-26 05:23:41 -0700

    init commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import React, { FC} from 'react';
+import React, { FC } from 'react';
+import { View, Text, Button } from 'react-native';
 // import { ThemeProvider } from './src/theme/ThemeContext';
 import AppNavigator from './src/routes/index';
 
-const App:FC = () => {
+const App: FC = () => {
   return (
     // <ThemeProvider>
-      <AppNavigator />
+    // <AppNavigator />
     // </ThemeProvider>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20 }}>Home Screen</Text>
+      <Text>Current Theme:</Text>
+      <Button title="Toggle Theme" />
+      <Button title="Go to Settings" />
+    </View>
   );
 };
 

fc331882652bdceaa7693306582a208143c81fbb
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-26 20:34:58 -0700

    [dev]-login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -72,7 +72,7 @@ const LoginScreen = () => {
           <ImageBackground
             source={BackgroundData[currentIndex].topImage}
             style={styles.backgroundImage}
-            resizeMode="cover"
+            resizeMode="contain"
           >
             <View style={styles.overlay}>
               <Animated.Text style={[styles.cornerText, learnStyle]}>
@@ -98,7 +98,7 @@ const LoginScreen = () => {
           <ImageBackground
             source={BackgroundData[currentIndex].bottomImage}
             style={styles.backgroundImage}
-            resizeMode="cover"
+            resizeMode="contain"
           >
           </ImageBackground>
       </View>

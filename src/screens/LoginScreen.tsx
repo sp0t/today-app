99c6e0345815e743f0d6322aefce8fa9e0141fee
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-26 18:41:18 -0700

    [dev]-login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -132,9 +132,10 @@ const styles = StyleSheet.create({
   imageArea: {
     width: width * 0.9, // 90% of screen width
     height: height * 0.4, // 40% of screen height
-    overflow: 'hidden',
     borderWidth: 1, // Optional: Add border for clarity
     borderColor: '#ccc',
+    backgroundColor: 'yellow',
+    
   },
   backgroundImage: {
     flex: 1,

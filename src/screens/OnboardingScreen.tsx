d0c36fcb9c00c40dd1f2699762c556e79bd332f5
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-28 09:05:25 -0700

    [dev]

--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -42,7 +42,7 @@ const OnboardingScreen = ({ navigation }: { navigation: any }) => {
                                 source={images.onboarding.OnboardingTop}
                                 style={baseStyles.bgImage}
                                 resizeMode="cover">
-                                <View style={{ marginTop: '30%' }}>
+                                <View style={{ marginTop: '50%' }}>
                                     <Text>Welcome to Today</Text>
                                     <Text style={styles.title}>Enter your email address</Text>
                                     <TextInput

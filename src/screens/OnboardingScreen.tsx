c540e8b86d3a091622426d19c9d7772da5e4d7c6
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-28 12:17:23 -0700

    [dev]

--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -161,7 +161,7 @@ const OnboardingScreen = ({ navigation }: { navigation: any }) => {
                         </View>
                         <View style={[baseStyles.bottomContainer, { alignItems: 'center', justifyContent: 'center' }]}>
                             <PrimaryInput
-                                style={{ marginTop: -25, width: '90%' }}
+                                style={{ marginTop: -30, width: '90%' }}
                                 value={formData.lastName}
                                 onChangeText={(text) => setFormData({ ...formData, lastName: text })}
                                 placeholder="Last name"

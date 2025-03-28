05e8ecfe59ffa79d2ae5b0bc4045db405c72ba2c
sp0t <ryan.paliukaitis.my@gmail.com>
2025-03-28 12:11:34 -0700

    [dev]

--- src/screens/OnboardingScreen.tsx
+++ src/screens/OnboardingScreen.tsx
@@ -134,28 +134,33 @@ const OnboardingScreen = ({ navigation }: { navigation: any }) => {
                                 source={images.onboarding.OnboardingTop}
                                 style={[baseStyles.bgImage, { alignItems: 'center' }]}
                                 resizeMode="cover"
-                                >
-                                <View style={styles.contentContainer}>
-                                    <SmallIcon source={images.onboarding.UserIcon} />
-                                    <Text style={styles.title}>What is your name?</Text>
-                                    <Text style={[styles.subtitle, { marginTop: 14 }]}>Your name is how others find you on Today.{'\n'}You can change this later.</Text>
-                                    <PrimaryInput
-                                        style={{ marginTop: 24 }}
-                                        value={formData.firstName}
-                                        onChangeText={(text) => setFormData({ ...formData, firstName: text })}
-                                        placeholder="First name"
-                                    />
-                                    <PrimaryInput
-                                        style={{ marginTop: 10 }}
-                                        value={formData.lastName}
-                                        onChangeText={(text) => setFormData({ ...formData, lastName: text })}
-                                        placeholder="Last name"
-                                    />
-                                </View>
+                            >
+                                {/* Pointer events for ImageBackground are disabled */}
+                                <View style={{ flex: 1, pointerEvents: 'none' }} />
                             </ImageBackground>
+                            <View style={styles.contentContainer}>
+                                <SmallIcon source={images.onboarding.UserIcon} />
+                                <Text style={styles.title}>What is your name?</Text>
+                                <Text style={[styles.subtitle, { marginTop: 14 }]}>
+                                    Your name is how others find you on Today. You can change this later.
+                                </Text>
+                                <PrimaryInput
+                                    style={{ marginTop: 24 }}
+                                    value={formData.firstName}
+                                    onChangeText={(text) => setFormData({ ...formData, firstName: text })}
+                                    placeholder="First name"
+                                />
+                                <PrimaryInput
+                                    style={{ marginTop: 10 }}
+                                    value={formData.lastName}
+                                    onChangeText={(text) => setFormData({ ...formData, lastName: text })}
+                                    placeholder="Last name"
+                                />
+                            </View>
                         </View>
                     </View>
                     <View style={{ flex: 0.1 }}>
+                        {/* Empty section */}
                     </View>
                     <View style={{ flex: 0.4 }}>
                         <View style={[baseStyles.bgImgContainer]}>
@@ -166,12 +171,16 @@ const OnboardingScreen = ({ navigation }: { navigation: any }) => {
                             />
                         </View>
                         <View style={[baseStyles.bottomContainer, { alignItems: 'center', justifyContent: 'center' }]}>
-                            <PrimaryButton title="Continue" style={[{ marginTop: '30%' }, !(formData.firstName && formData.lastName) && styles.buttonDisabled]}
+                            <PrimaryButton
+                                title="Continue"
+                                style={[{ marginTop: '30%' }, !(formData.firstName && formData.lastName) && styles.buttonDisabled]}
                                 disabled={!(formData.firstName && formData.lastName)}
-                                onPress={() => sliderRef.current?.goToSlide(3)} />
+                                onPress={() => sliderRef.current?.goToSlide(3)}
+                            />
                         </View>
                     </View>
                 </View>
+
             ),
         },
         {

d1919f68d81d4f4981f284cf6d9e53d19b8883e9
--global <ryan.paliukaitis.my@gmail.com>
2025-04-01 09:11:08 +0300

    [dev]

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React, { FC } from 'react';
 import { ThemeProvider } from './src/theme/ThemeContext';
 import AppNavigator from './src/routes/index';
+import 'fast-text-encoding';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+import { PrivyProvider } from '@privy-io/expo';
 
 const App: FC = () => {
   return (
     <ThemeProvider>
-      <AppNavigator />
+      <PrivyProvider appId="cm76p206400ey5b2sgwsn2acf" clientId="client-WY5gxmNC7Y288p2SAnCrTgb5kKnfAhB63h4RgUyZNaNM3">
+        <AppNavigator />
+      </PrivyProvider>
     </ThemeProvider>
   );
 };
